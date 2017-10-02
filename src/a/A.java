4e35d858be9418a417ae9b278905d772cce8e775
Nakhon41 <jo_sometime@hotmail.com>
2017-10-02 10:55:59 +0700

    Create Any input

---
+++ src/a/A.java
@@ -0,0 +1,31 @@
+package a;
+
+public class A {
+	double sarary;
+	double sale;
+	double result =0;
+	
+	public A(double salary, double com) {
+		this.sarary=salary;
+		this.sale =com;
+	}
+	public double find() {
+		if(this.sale>50000) {
+			this.result=(this.sale*(0.03))+this.sarary;
+		}else if(this.sale>25000) {
+			this.result=(this.sale*(0.02))+this.sarary;
+		}else
+		{
+		this.result=(this.sale*(0.01))+this.sarary;
+			if(this.result < 18000 ) {
+				this.result = 18000 ;
+		}
+			if(this.sarary < 15000 && this.sale > 100000)
+			{
+				this.result += 3000;
+			}
+		
+		return this.result;
+		}
+	}
+}
